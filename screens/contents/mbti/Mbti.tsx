64b33e32949f52fb775140898eb2f55165f4c1ff
HyeJung Kang <116064204+KHJmarina@users.noreply.github.com>
2023-06-30 13:44:24 +0900

    Add files via upload

---
+++ screens/contents/mbti/Mbti.tsx
@@ -0,0 +1,26 @@
+import Box from '@mui/material/Box';
+import { observer } from "mobx-react-lite";
+import { useEffect, useState } from 'react';
+import { useStores } from "src/models/root-store/root-store-context"
+import { useTheme } from '@mui/material';
+
+/**
+ * ## Mbti 설명
+ *
+ */
+export const Mbti = observer(() => {
+
+  const rootStore = useStores();
+  const { loadingStore } = rootStore;
+  const theme = useTheme();
+
+  return (
+    <>
+      <Box>
+        Mbti Screen
+      </Box>
+    </>
+  );
+});
+
+export default Mbti;
